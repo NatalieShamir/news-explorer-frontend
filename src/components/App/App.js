9c2f73748c9650c8ff64106cedab0c679c3e6783
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2023-05-24 10:30:23 +0300

    add import statements for components and JSX

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,33 @@
 import './App.css';
+import Main from "./Main";
+import SavedNews from "./SavedNews";
+import Header from "./Header";
+import Navigation from "./Navigation";
+import SearchForm from "./SearchForm";
+import NewsCardList from "./NewsCardList";
+import NewsCard from "./NewsCard";
+import About from "./About";
+import Preloader from "./Preloader";
+import Footer from "./Footer";
+import PopupWithForm from "./PopupWithForm";
+import SavedNewsHeader from "./SavedNewsHeader";
+
 
 function App() {
   return (
     <div className="App">
-
+      <Main />
+      <SavedNews />
+      <Header />
+      <Navigation />
+      <SearchForm />
+      <NewsCardList />
+      <NewsCard />
+      <About />
+      <Preloader />
+      <Footer />
+      <PopupWithForm />
+      <SavedNewsHeader />
     </div>
   );
 }
