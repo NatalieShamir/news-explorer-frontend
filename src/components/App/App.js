ed6b54e02a4404adbcc1c8d3e07dfe4394ba6407
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2023-05-24 13:07:19 +0300

    correct relative paths of import statments

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,16 @@
-import './App.css';
-import Main from "./Main";
-import SavedNews from "./SavedNews";
-import Header from "./Header";
-import Navigation from "./Navigation";
-import SearchForm from "./SearchForm";
-import NewsCardList from "./NewsCardList";
-import NewsCard from "./NewsCard";
-import About from "./About";
-import Preloader from "./Preloader";
-import Footer from "./Footer";
-import PopupWithForm from "./PopupWithForm";
-import SavedNewsHeader from "./SavedNewsHeader";
+import '../App.css';
+import Main from "../Main";
+import SavedNews from "../SavedNews";
+import Header from "../Header";
+import Navigation from "../Navigation";
+import SearchForm from "../SearchForm";
+import NewsCardList from "../NewsCardList";
+import NewsCard from "../NewsCard";
+import About from "../About";
+import Preloader from "../Preloader";
+import Footer from "../Footer";
+import PopupWithForm from "../PopupWithForm";
+import SavedNewsHeader from "../SavedNewsHeader";
 
 
 function App() {
