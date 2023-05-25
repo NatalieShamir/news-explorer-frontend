cc0c5ceda10c3ce10b4ae9614fc953dd62f221e7
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2023-05-25 12:09:24 +0300

    create component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import GitHubLogo from "../../images/github.svg";
+import FacebookLogo from "../../images/facebook.svg";
+
+export default function Footer() {
+    const handlePracticumClick = () => {
+        window.location.href = 'https://practicum.com/en-isr/';
+    };
+    const handleGitHubClick = () => {
+        window.location.href = 'https://github.com/';
+    };
+    const handleFacebookClick = () => {
+        window.location.href = 'https://www.facebook.com/';
+    };
+    return (
+        <footer className="footer">
+            <p className="footer__copyright">
+                &#169; {new Date().getFullYear()}Supersite, Powered by News API</p>
+            <Link to={"/"} className="footer__home">
+                Home
+            </Link>
+            <button onClick={handlePracticumClick}>Practicum</button>
+            <button onClick={handleGitHubClick}><img src={GitHubLogo} /></button>
+            <button onClick={handleFacebookClick}><img src={FacebookLogo} /></button>
+        </footer>
+    );
+}
