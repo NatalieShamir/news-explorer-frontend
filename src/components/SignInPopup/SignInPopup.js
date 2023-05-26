42fde733f4d2fc6a6d7c667e32096b4cb0a62ed0
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2023-05-26 17:53:37 +0300

    create component

---
+++ src/components/SignInPopup/SignInPopup.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+function Signin({ onSignin }) {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSignin = (e) => {
+        e.preventDefault();
+        onSignin(email, password)
+    }
+
+    return (
+        <div className="signin">
+            <form onSubmit={handleSignin} className="signin__form" >
+                <h3 className="signin__welcome">Sign in</h3>
+                <label htmlFor="email"></label>
+                <input type="email" id="email-input" className="signin__input" placeholder="Email"
+                    value={email} onChange={e => setEmail(e.target.value)} />
+                <label htmlFor="password"></label>
+                <input type="password" id="password-input" className="signin__input" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
+                <div className="signin__button-container">
+                    <button type="submit" className="signin__link">
+                        Sign in
+                    </button>
+                </div>
+            </form>
+
+            <div className="signin__signup">
+                <p className="signin__signup-text">or</p>
+                <Link style={{ textDecoration: 'none' }} to="/signup" >
+                    <p className="signin__signup-link"> Sign up</p>
+                </Link>
+            </div>
+        </div>
+
+    )
+}
+
+export { Signin }
