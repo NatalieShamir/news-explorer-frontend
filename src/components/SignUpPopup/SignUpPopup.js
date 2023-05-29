99cf9f8bf0193fedb56aed350f220253625c3719
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2023-05-29 15:17:58 +0300

    create component

---
+++ src/components/SignUpPopup/SignUpPopup.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+function Signup({ onSignup }) {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [username, setUsername] = useState("");
+
+    const handleSignup = (e) => {
+        e.preventDefault();
+        onSignup(email, password, username)
+    }
+
+    return (
+        <div className="signup">
+            <form onSubmit={handleSignup} className="signup__form">
+                <h3 className="signup__welcome">
+                    Sign up
+                </h3>
+                <label htmlFor="email"></label>
+                <input name="email" type="email" id="email-input" className="signup__input" placeholder="Enter email" value={email}
+                    onChange={e => setEmail(e.target.value)} />
+                <label htmlFor="password"></label>
+                <input name="password" type="password" id="password-input" className="signup__input" placeholder="Enter password" value={password}
+                    onChange={e => setPassword(e.target.value)} />
+                <label htmlFor="username"></label>
+                <input name="username" type="text" id="username-input" className="signup__input" placeholder="Enter your username" value={username}
+                    onChange={e => setUsername(e.target.value)} />
+                <div className="signup__button-container">
+                    <button className="signup__link">Sign up</button>
+                </div>
+            </form>
+
+            <div className="signin__signup">
+                <p className="signin__signup-text">or</p>
+                <Link style={{ textDecoration: 'none' }} to="/signin" >
+                    <p className="signin__signup-link"> Sign in</p>
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export { Signup };
