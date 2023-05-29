4b48c218063547e8d43b89ec9ad7c6efaed64ae0
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2023-05-29 15:00:14 +0300

    change placeholders

--- src/components/SignInPopup/SignInPopup.js
+++ src/components/SignInPopup/SignInPopup.js
@@ -15,10 +15,10 @@ function Signin({ onSignin }) {
             <form onSubmit={handleSignin} className="signin__form" >
                 <h3 className="signin__welcome">Sign in</h3>
                 <label htmlFor="email"></label>
-                <input type="email" id="email-input" className="signin__input" placeholder="Email"
+                <input type="email" id="email-input" className="signin__input" placeholder="Enter email"
                     value={email} onChange={e => setEmail(e.target.value)} />
                 <label htmlFor="password"></label>
-                <input type="password" id="password-input" className="signin__input" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
+                <input type="password" id="password-input" className="signin__input" placeholder="Enter password" value={password} onChange={e => setPassword(e.target.value)} />
                 <div className="signin__button-container">
                     <button type="submit" className="signin__link">
                         Sign in
