50c71e6e21f57db66989bfae85bfb40354ff81a5
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2023-06-08 09:41:26 +0300

    add container for card text elements

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -6,10 +6,13 @@ function NewsCard(props) {
         <div className="card">
             <img src={props.cardImage} className="card__image" alt="news card" />
             <button className="card__button"></button>
-            <p className="card__date">{props.date}</p>
-            <h2 className="card__title">{props.title}</h2>
-            <p className="card__text">{props.text}</p>
-            <p className="card__website">{props.website}</p>
+            <div className="card__container">
+                <p className="card__date">{props.date}</p>
+                <h2 className="card__title">{props.title}</h2>
+                <p className="card__text">{props.text}</p>
+                <p className="card__website">{props.website}</p>
+            </div>
+
         </div>
     )
 }
