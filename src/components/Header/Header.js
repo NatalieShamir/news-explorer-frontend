0bd73557751de471a88b3d879e3c20beeb46d07c
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2023-06-09 16:07:37 +0300

    remove code for creation of seperate component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 import { Link } from 'react-router-dom';
 import "../Header/Header.css"
 
-function Header({ isLoggedIn, onSignIn, onSignOut }) {
+function Header({ onSignIn }) {
 
     return (
         <header className="header">
@@ -14,25 +14,14 @@ function Header({ isLoggedIn, onSignIn, onSignOut }) {
             />
 
             <div className="header__container">
-                {isLoggedIn ? (
-                    <div>
-                        <Link to={"/"} className="header__home">
-                            Home
-                        </Link>
-                        <Link to={"/"} className="header__articles">Saved articles</Link>
-                        <button className="header__button-signout" onClick={onSignOut}></button>
-                    </div>
-
-                ) : (
-                    <div className="header__container-items">
-                        <Link to={"/"} className="header__home">
-                            Home
-                        </Link>
-                        <button className="header__button-signin" onClick={onSignIn}>Sign in</button>
-                    </div>
-                )}
-            </div>
-        </header>
+                <div className="header__container-items">
+                    <Link to={"/"} className="header__home">
+                        Home
+                    </Link>
+                    <button className="header__button-signin" onClick={onSignIn}>Sign in</button>
+                </div>
+            </div >
+        </header >
     );
 }
 
