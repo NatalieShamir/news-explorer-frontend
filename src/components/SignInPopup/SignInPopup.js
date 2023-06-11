beaba4969c06b6b700865ed74ebfb1fa5eb527ab
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2023-06-11 22:41:40 +0300

    add functionality

--- src/components/SignInPopup/SignInPopup.js
+++ src/components/SignInPopup/SignInPopup.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import "../SignInPopup/SignInPopup.css";
 
-function Signin({ onSignin }) {
+function Signin({ onSignin, isOpen, onClose }) {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
@@ -11,30 +12,33 @@ function Signin({ onSignin }) {
     }
 
     return (
-        <div className="signin">
-            <form onSubmit={handleSignin} className="signin__form" >
-                <h3 className="signin__welcome">Sign in</h3>
-                <label htmlFor="email" className="signin__label">Email</label>
-                <input type="email" id="email-input" className="signin__input" placeholder="Enter email"
-                    value={email} onChange={e => setEmail(e.target.value)} />
-                <label htmlFor="password" className="signin__label">Password</label>
-                <input type="password" id="password-input" className="signin__input" placeholder="Enter password" value={password} onChange={e => setPassword(e.target.value)} />
-                <div className="signin__button-container">
-                    <button type="submit" className="signin__link">
+        <div className={`${`signin `} ${isOpen ? "signin_open" : ""}`}>
+            <div className="signin__container">
+                <button
+                    onClick={onClose}
+                    type="button"
+                    aria-label="close"
+                    className="signin__close-button"
+                />
+                <form onSubmit={handleSignin} className="signin__form" >
+                    <h3 className="signin__form-title">Sign in</h3>
+                    <label htmlFor="email" className="signin__form-label">Email</label>
+                    <input type="email" id="email-input" className="signin__form-input" placeholder="Enter email"
+                        value={email} onChange={e => setEmail(e.target.value)} />
+                    <label htmlFor="password" className="signin__form-label">Password</label>
+                    <input type="password" id="password-input" className="signin__form-input" placeholder="Enter password" value={password} onChange={e => setPassword(e.target.value)} />
+                    <button type="submit" className="signin__form-button signin__form-button_disabled">
                         Sign in
                     </button>
-                </div>
-            </form>
-
-            <div className="signin__signup">
-                <p className="signin__signup-text">or</p>
-                <Link style={{ textDecoration: 'none' }} to="/signup" >
-                    <p className="signin__signup-link"> Sign up</p>
-                </Link>
+                    <div className="signin__signup">
+                        <p className="signin__signup-text">or</p>
+                        <Link style={{ textDecoration: 'none' }} to="/signup" >
+                            <p className="signin__signup-link"> Sign up</p>
+                        </Link>
+                    </div>
+                </form>
             </div>
         </div>
-
     )
 }
-
-export { Signin }
+export default Signin; 
--- src/components/SignUpPopup/SignUpPopup.js
+++ src/components/SignUpPopup/SignUpPopup.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import "../SignUpPopup/SignUpPopup.css";
 
-function Signup({ onSignup }) {
+function Signup({ onSignup, isOpen, onClose }) {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [username, setUsername] = useState("");
@@ -10,35 +11,38 @@ function Signup({ onSignup }) {
         e.preventDefault();
         onSignup(email, password, username)
     }
-
     return (
-        <div className="signup">
-            <form onSubmit={handleSignup} className="signup__form">
-                <h3 className="signup__welcome">
-                    Sign up
-                </h3>
-                <label htmlFor="email" className="signup__label">Email</label>
-                <input name="email" type="email" id="email-input" className="signup__input" placeholder="Enter email" value={email}
-                    onChange={e => setEmail(e.target.value)} />
-                <label htmlFor="password" className="signup__label">Password</label>
-                <input name="password" type="password" id="password-input" className="signup__input" placeholder="Enter password" value={password}
-                    onChange={e => setPassword(e.target.value)} />
-                <label htmlFor="username" className="signup__label">Username</label>
-                <input name="username" type="text" id="username-input" className="signup__input" placeholder="Enter your username" value={username}
-                    onChange={e => setUsername(e.target.value)} />
-                <div className="signup__button-container">
-                    <button className="signup__link">Sign up</button>
-                </div>
-            </form>
-
-            <div className="signin__signup">
-                <p className="signin__signup-text">or</p>
-                <Link style={{ textDecoration: 'none' }} to="/signin" >
-                    <p className="signin__signup-link"> Sign in</p>
-                </Link>
+        <div className={`${`signup `} ${isOpen ? "signup_open" : ""}`}>
+            <div className="signup__container">
+                <button
+                    onClick={onClose}
+                    type="button"
+                    aria-label="close"
+                    className="signup__close-button"
+                />
+                <form onSubmit={handleSignup} className="signup__form" >
+                    <h3 className="signup__form-title">Sign up</h3>
+                    <label htmlFor="email" className="signup__form-label">Email</label>
+                    <input type="email" id="email-input" className="signup__form-input" placeholder="Enter email"
+                        value={email} onChange={e => setEmail(e.target.value)} />
+                    <label htmlFor="password" className="signup__form-label">Password</label>
+                    <input type="password" id="password-input" className="signup__form-input" placeholder="Enter password" value={password} onChange={e => setPassword(e.target.value)} />
+                    <label htmlFor="username" className="signup__label">Username</label>
+                    <input name="username" type="text" id="username-input" className="signup__input" placeholder="Enter your username" value={username}
+                        onChange={e => setUsername(e.target.value)} />
+                    <button type="submit" className="signup__form-button signup__form-button_disabled">
+                        Sign in
+                    </button>
+                    <div className="signup__signin">
+                        <p className="signup__signin-text">or</p>
+                        <Link style={{ textDecoration: 'none' }} to="/signin" >
+                            <p className="signup__signin-link"> Sign in</p>
+                        </Link>
+                    </div>
+                </form>
             </div>
         </div>
     )
-}
-
-export { Signup };
+}/* 
+*/
+export default Signup;
