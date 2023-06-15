e1992d39e0d6ddac8debc6c67a1994d5bbac178a
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2023-06-15 12:48:50 +0300

    change Link to Nav Link and fix routing

--- src/components/SavedNewsHeader/SavedNewsHeader.js
+++ src/components/SavedNewsHeader/SavedNewsHeader.js
@@ -1,22 +1,24 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import "../SavedNewsHeader/SavedNewsHeader.css";
-import headerLogo from "../../images/header-min.svg";
+import savedNewsHeaderLogo from "../../images/saved-news-header-logo.svg";
 import logout from "../../images/logout.svg";
 
 function SavedNewsHeader() {
     return (
         <div className="saved-news-header">
-            <img
-                className="saved-news-header__image"
-                src={headerLogo}
-                alt="Project title- NewsExplorer"
-            />
+            <NavLink to={"/"} className="saved-news-header__logo">
+                <img
+                    src={savedNewsHeaderLogo}
+                    alt="Project title- NewsExplorer"
+                />
+            </NavLink>
+
             <div className="saved-news-header__container">
-                <Link to={"/"} className="saved-news-header__home">
+                <NavLink to={"/"} className="saved-news-header__home">
                     Home
-                </Link>
-                <Link to={"/"} className="saved-news-header__articles">Saved articles</Link>
+                </NavLink>
+                <NavLink to={"/saved-news"} className="saved-news-header__articles">Saved articles</NavLink>
                 <button className="saved-news-header__button">
                     <div className="saved-news-header__button-items">
                         <div className="saved-news-header__user">Elise</div>
