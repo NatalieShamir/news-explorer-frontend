81de5b62459988dc4333dc504e9a95dd5a82c33a
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2023-06-15 11:26:46 +0300

    remove modifier from mark-up

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,18 +1,18 @@
 import React from "react";
-import "../NewsCard/NewsCard.css"
+import "../NewsCard/NewsCard.css";
 
 function NewsCard(props) {
+
     return (
         <div className="card">
             <img src={props.cardImage} className="card__image " alt="news card" />
-            <button className="card__button card__button_marked"></button>
+            <button className="card__button"></button>
             <div className="card__container">
                 <p className="card__date">{props.date}</p>
                 <h2 className="card__title">{props.title}</h2>
                 <p className="card__text">{props.text}</p>
                 <p className="card__website">{props.website}</p>
             </div>
-
         </div>
     )
 }
