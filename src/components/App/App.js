882b71972382cda931d27f44e2a6dd23e6e7d66b
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2023-06-19 09:59:28 +0300

    remove redundant code

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { useEffect } from "react";
 import {
   Route,
   Routes,
@@ -10,28 +9,6 @@ import Footer from "../Footer/Footer";
 import SavedNews from "../SavedNews/SavedNews";
 
 function App() {
-
-  const [isSignInPopupOpen, setIsSignInPopupOpen] = React.useState(false);
-  const [isSignUpPopupOpen, setIsSignUpPopupOpen] = React.useState(false);
-
-  function handleSignUpClick() {
-    setIsSignUpPopupOpen(true);
-  }
-
-  function closeAllPopups() {
-    setIsSignInPopupOpen(false);
-    setIsSignUpPopupOpen(false);
-  }
-
-  useEffect(() => {
-    function closeByEscape(e) {
-      if (e.key === "Escape") {
-        closeAllPopups();
-      }
-    }
-    document.addEventListener("keydown", closeByEscape);
-    return () => document.removeEventListener("keydown", closeByEscape);
-  }, []);
 
   return (
     <div className="app">
