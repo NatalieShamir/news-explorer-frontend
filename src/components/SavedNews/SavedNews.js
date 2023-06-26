6b2b1e1f122a0bc6d9be6e2651a52b09c0fb3b1b
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2023-06-26 11:42:32 +0300

    add Header component to JSX

--- src/components/SavedNews/SavedNews.js
+++ src/components/SavedNews/SavedNews.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import NewsCard from "../NewsCard/NewsCard.js";
 import { api } from "../../utils/Api.js";
-import "../SavedNews/SavedNews.css"
+import "../SavedNews/SavedNews.css";
+import Header from "../Header/Header.js";
 
 function SavedNews({ code }) {
     const [cards, setCards] = useState([]);
@@ -18,16 +19,19 @@ function SavedNews({ code }) {
     const cardComponents = cards.map((card) => <NewsCard {...card} key={card.id} />);
 
     return (
-        <div className="saved-news">
-            <h2 className="saved-news__heading">Saved articles</h2>
-            <h3 className="saved-news__subheading">Elise, you have 5 saved articles</h3>
-            <p className="saved-news__keywords">By keywords:
-                <span className="saved-news__keywords-bold" style={{ fontWeight: 'bold' }}>Nature, Yellowstone, and 2 other</span>
-            </p>
-            <ul className="saved-news__card-list">
-                {React.Children.toArray(cardComponents)}
-            </ul><div>{code}</div>
-        </div >
+        <>
+            <Header />
+            <div className="saved-news">
+                <h2 className="saved-news__heading">Saved articles</h2>
+                <h3 className="saved-news__subheading">Elise, you have 5 saved articles</h3>
+                <p className="saved-news__keywords">By keywords:
+                    <span className="saved-news__keywords-bold" style={{ fontWeight: 'bold' }}>Nature, Yellowstone, and 2 other</span>
+                </p>
+                <ul className="saved-news__card-list">
+                    {React.Children.toArray(cardComponents)}
+                </ul><div>{code}</div>
+            </div >
+        </>
     )
 }
 
