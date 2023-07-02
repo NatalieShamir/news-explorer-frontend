10a1b63c50742945a3929e070e6f9bf0cd9eb918
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2023-07-02 11:22:38 +0300

    change JSX and props accordingly

--- src/components/PopupWithForm/PopupWithForm.js
+++ src/components/PopupWithForm/PopupWithForm.js
@@ -3,27 +3,14 @@ import React from "react";
 function PopupWithForm({
     name,
     isOpen,
-    onClose,
-    title,
-    children,
-    onSubmit,
-
+    children
 }) {
     return (
         <div
             className={`${`popup popup_type_${name}`} ${isOpen ? "popup_open" : ""}`}
         >
             <div className="popup__container">
-                <button
-                    onClick={onClose}
-                    type="button"
-                    aria-label="close"
-                    className="popup__close-button"
-                />
-                <form name={name} className="popup__form" onSubmit={onSubmit}>
-                    <h3 className="popup__form-title">{title}</h3>
-                    {children}
-                </form>
+                {children}
             </div>
         </div>
     );
