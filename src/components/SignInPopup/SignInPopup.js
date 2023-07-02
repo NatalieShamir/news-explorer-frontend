91b2c872cb14c74ae06bdc1958a4dfbad43fe80f
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2023-07-02 11:24:26 +0300

    add to JSX and update props accordingly

--- src/components/SignInPopup/SignInPopup.js
+++ src/components/SignInPopup/SignInPopup.js
@@ -5,7 +5,7 @@ import "../PopupWithForm/PopupWithForm.css";
 import { Link } from 'react-router-dom';
 
 
-function Signin({ isOpen, onClose }) {
+function Signin({ isOpen, onClose, onSubmit }) {
 
     const [isSignUpPopupOpen, setIsSignUpPopupOpen] =
         React.useState(false);
@@ -21,44 +21,50 @@ function Signin({ isOpen, onClose }) {
     return (
         <>
             <PopupWithForm
-                name="signin"
-                title="Sign in"
                 isOpen={isOpen}
                 onClose={onClose}
             >
                 {" "}
-                < fieldset className="popup__form-fieldset" >
-                    <label htmlFor="email" className="popup__form-label">Email</label>
-                    <input
-                        type="email"
-                        id="email-input"
-                        className="popup__form-input"
-                        placeholder="Enter email"
-                        required
-                    />
-                    <span id="email-input-error" className="popup__form-error"></span>
-                </fieldset >
-                <fieldset className="popup__form-fieldset">
-                    <label htmlFor="password" className="popup__form-label">Password</label>
-                    <input
-                        type="password"
-                        id="password-input"
-                        className="popup__form-input"
-                        placeholder="Enter password"
-                        required
-                    />
-                    <span id="password-input-error" className="popup__form-error"></span>
-                </fieldset>
-                {" "}
-                <button type="submit" className="popup__form-button popup__form-button_disabled">
-                    Sign in
-                </button>
-                <div className="popup__form-link-container">
-                    <p className="popup__form-link-text">or</p>
-                    <Link style={{ textDecoration: 'none' }} onClick={handleSignUpClick}>
-                        <p className="popup__form-link">Sign up</p>
-                    </Link>
-                </div>
+                <button
+                    onClick={onClose}
+                    type="button"
+                    aria-label="close"
+                    className="popup__close-button-signin"
+                />
+                <form name="signin" className="popup__form" onSubmit={onSubmit}>
+                    <h3 className="popup__form-title">Sign in</h3>
+                    < fieldset className="popup__form-fieldset" >
+                        <label htmlFor="email" className="popup__form-label">Email</label>
+                        <input
+                            type="email"
+                            id="email-input"
+                            className="popup__form-input"
+                            placeholder="Enter email"
+                            required
+                        />
+                        <span id="email-input-error" className="popup__form-error"></span>
+                    </fieldset >
+                    <fieldset className="popup__form-fieldset">
+                        <label htmlFor="password" className="popup__form-label">Password</label>
+                        <input
+                            type="password"
+                            id="password-input"
+                            className="popup__form-input"
+                            placeholder="Enter password"
+                            required
+                        />
+                        <span id="password-input-error" className="popup__form-error"></span>
+                    </fieldset>
+                    <button type="submit" className="popup__form-button popup__form-button_disabled">
+                        Sign in
+                    </button>
+                    <div className="popup__form-link-container">
+                        <p className="popup__form-link-text">or</p>
+                        <Link style={{ textDecoration: 'none' }} onClick={handleSignUpClick}>
+                            <p className="popup__form-link">Sign up</p>
+                        </Link>
+                    </div>
+                </form>
             </PopupWithForm >
             <Signup isOpen={isSignUpPopupOpen} onClose={closeSignUpPopup} />
         </>
--- src/components/SignUpPopup/SignUpPopup.js
+++ src/components/SignUpPopup/SignUpPopup.js
@@ -4,7 +4,7 @@ import PopupWithForm from '../PopupWithForm/PopupWithForm';
 import "../PopupWithForm/PopupWithForm.css";
 import RegistrationSuccessful from '../RegistrationSuccessful/RegistrationSuccessful';
 
-function Signup({ isOpen, onClose }) {
+function Signup({ isOpen, onClose, onSubmit }) {
     const [isRegistrationSuccessfulPopupOpen, setIsRegistrationSuccessfulPopupOpen] =
         React.useState(false);
 
@@ -19,55 +19,61 @@ function Signup({ isOpen, onClose }) {
     return (
         <>
             <PopupWithForm
-                name="signup"
-                title="Sign up"
                 isOpen={isOpen}
                 onClose={onClose}
             >
                 {" "}
-                < fieldset className="popup__form-fieldset" >
-                    <label htmlFor="email" className="popup__form-label">Email</label>
-                    <input
-                        type="email"
-                        id="email-input"
-                        className="popup__form-input"
-                        placeholder="Enter email"
-                        required
-                    />
-                    <span id="email-input-error" className="popup__form-error"></span>
-                </fieldset >
-                <fieldset className="popup__form-fieldset">
-                    <label htmlFor="password" className="popup__form-label">Password</label>
-                    <input
-                        type="password"
-                        id="password-input"
-                        className="popup__form-input"
-                        placeholder="Enter password"
-                        required
-                    />
-                    <span id="password-input-error" className="popup__form-error"></span>
-                </fieldset>
-                < fieldset className="popup__form-fieldset" >
-                    <label htmlFor="username" className="popup__form-label">Username</label>
-                    <input
-                        type="text"
-                        id="username-input"
-                        className="popup__form-input"
-                        placeholder="Enter your username"
-                        required
-                    />
-                    <span id="email-input-error" className="popup__form-error"></span>
-                </fieldset >
-                {" "}
-                <button type="submit" className="popup__form-button" onClick={handleSignInClick}>
-                    Sign up
-                </button>
-                <div className="popup__form-link-container">
-                    <p className="popup__form-link-text">or</p>
-                    <Link style={{ textDecoration: 'none' }} >
-                        <p className="popup__form-link">Sign in</p>
-                    </Link>
-                </div>
+                <button
+                    onClick={onClose}
+                    type="button"
+                    aria-label="close"
+                    className="popup__close-button-signup"
+                />
+                <form name="signin" className="popup__form" onSubmit={onSubmit}>
+                    <h3 className="popup__form-title">Sign up</h3>
+                    < fieldset className="popup__form-fieldset" >
+                        <label htmlFor="email" className="popup__form-label">Email</label>
+                        <input
+                            type="email"
+                            id="email-input"
+                            className="popup__form-input"
+                            placeholder="Enter email"
+                            required
+                        />
+                        <span id="email-input-error" className="popup__form-error"></span>
+                    </fieldset >
+                    <fieldset className="popup__form-fieldset">
+                        <label htmlFor="password" className="popup__form-label">Password</label>
+                        <input
+                            type="password"
+                            id="password-input"
+                            className="popup__form-input"
+                            placeholder="Enter password"
+                            required
+                        />
+                        <span id="password-input-error" className="popup__form-error"></span>
+                    </fieldset>
+                    < fieldset className="popup__form-fieldset" >
+                        <label htmlFor="username" className="popup__form-label">Username</label>
+                        <input
+                            type="text"
+                            id="username-input"
+                            className="popup__form-input"
+                            placeholder="Enter your username"
+                            required
+                        />
+                        <span id="email-input-error" className="popup__form-error"></span>
+                    </fieldset >
+                    <button type="submit" className="popup__form-button" onClick={handleSignInClick}>
+                        Sign up
+                    </button>
+                    <div className="popup__form-link-container">
+                        <p className="popup__form-link-text">or</p>
+                        <Link style={{ textDecoration: 'none' }} >
+                            <p className="popup__form-link">Sign in</p>
+                        </Link>
+                    </div>
+                </form>
             </PopupWithForm >
             <RegistrationSuccessful isOpen={isRegistrationSuccessfulPopupOpen} onClose={closeRegistrationSuccessfulPopup} />
         </>
