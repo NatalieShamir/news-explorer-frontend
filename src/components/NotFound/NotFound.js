f75d94835a630e3aef64f2ee4cfcd0646b5c87c3
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2023-07-02 11:20:41 +0300

    change file format for image and add alt text

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "../NotFound/NotFound.css";
-import notFound from "../../images/not-found.png";
+import notFound from "../../images/not-found.svg";
 
 function NotFound() {
     return (
         <div className="not-found">
-            <img className="not-found__image" src={notFound}></img>
+            <img className="not-found__image" alt="Magnifying glass with frowning smiley face" src={notFound}></img>
             <h2 className="not-found__heading">Nothing found</h2>
             <p className="not-found__text">Sorry, but nothing matched
                 your search terms.</p>
