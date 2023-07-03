b34abe2cf9b98b8f1a7e0a9181bfa1ecce006feb
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2023-07-03 14:22:28 +0300

    change div tag to section

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -18,13 +18,13 @@ function SearchResults({ code }) {
   const cardComponents = cards.slice(0, 3).map((card) => <NewsCard {...card} key={card.id} />);
 
   return (
-    <div className="search-results">
+    <section className="search-results">
       <h2 className="search-results__heading">Search results</h2>
       <ul className="search-results__card-list">
         {React.Children.toArray(cardComponents)}
       </ul><div>{code}</div>
       <button type="button" className="search-results__button">Show more</button>
-    </div>
+    </section>
   )
 }
 
