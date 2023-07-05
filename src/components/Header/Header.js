79eaf8127c5ae04c908469470855439b69cc7c08
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2023-07-05 14:24:57 +0300

    refactor: create Navigation component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,52 +1,11 @@
-import headerLogo from "../../images/header-min.svg";
-import React, { useEffect } from "react";
-import { NavLink } from 'react-router-dom';
+import React from "react";
 import "../Header/Header.css";
-import SignIn from "../SignInPopup/SignInPopup";
+import Navigation from "../Navigation/Navigation";
 
 function Header() {
-
-    const [isSignInPopupOpen, setIsSignInPopupOpen] =
-        React.useState(false);
-
-    function handleSignInClick() {
-        setIsSignInPopupOpen(true);
-    }
-
-    function closeSignInPopup() {
-        setIsSignInPopupOpen(false);
-    }
-
-    useEffect(() => {
-        function closeByEscape(e) {
-            if (e.key === "Escape") {
-                closeSignInPopup();
-            }
-        }
-        document.addEventListener("keydown", closeByEscape);
-        return () => document.removeEventListener("keydown", closeByEscape);
-    }, []);
-
     return (
         <header className="header">
-            <NavLink to={"/"} className="header__logo">
-                <img
-                    className="header__logo-image"
-                    src={headerLogo}
-                    alt="Project title- NewsExplorer"
-                />
-            </NavLink>
-
-            <div className="header__container">
-                <div className="header__container-items">
-                    <NavLink to={"/"} className="header__home">
-                        Home
-                    </NavLink>
-                    <button className="header__button-signin" onClick={handleSignInClick}>Sign in</button>
-                    <button className="header__button-menu"></button>
-                </div>
-            </div >
-            <SignIn isOpen={isSignInPopupOpen} onClose={closeSignInPopup} />
+            <Navigation />
         </header >
     );
 }
