ce1e53dfb046841dd566d0c21f532fe7a244e2a8
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2023-07-09 14:15:34 +0300

    code hamburger menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,35 @@
 import React from "react";
+import { Link } from 'react-router-dom';
+import headerLogo from "../../images/header-min.svg";
 import "../Header/Header.css";
 import Navigation from "../Navigation/Navigation";
+import hamburgerMenuOpen from "../../images/menu.svg";
+import hamburgerMenuClose from "../../images/close-min.svg";
 
 function Header() {
+    const [isNavOpen, setIsNavOpen] = React.useState(false);
+
+    function handleToggleMenu() {
+        setIsNavOpen(!isNavOpen)
+    }
+
     return (
-        <header className="header">
-            <Navigation />
+        <header className={isNavOpen ? "header_hamburger_menu_open" : "header"}>
+            <div className="header__container">
+                <Link to={"/"} className="header__logo">
+                    <img
+                        className="header__logo-image"
+                        src={headerLogo}
+                        alt="Project title- NewsExplorer"
+                    />
+                </Link>
+                <button className="header__hamburger-menu-toggle" onClick={handleToggleMenu}>
+                    {isNavOpen ? <img src={hamburgerMenuClose} /> : <img src={hamburgerMenuOpen} />}
+                </button>
+            </div>
+            <Navigation
+                isNavOpen={isNavOpen}
+            />
         </header >
     );
 }
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,18 +1,11 @@
 import React from "react";
 import { Link } from 'react-router-dom';
-import headerLogo from "../../images/header-min.svg";
 import "../Navigation/Navigation.css";
 
-function Navigation() {
+function Navigation({ isNavOpen }) {
+
     return (
-        <nav className="nav">
-            <Link to={"/"} className="nav__logo">
-                <img
-                    className="nav__logo-image"
-                    src={headerLogo}
-                    alt="Project title- NewsExplorer"
-                />
-            </Link>
+        <nav className={`${`nav`} ${isNavOpen ? "nav_open" : ""}`}>
             <ul className="nav__items">
                 <li className="nav__item">
                     <Link to={"/"} className="nav__home">
@@ -22,11 +15,8 @@ function Navigation() {
                 <li className="nav__item">
                     <button className="nav__button-signin">Sign in</button>
                 </li>
-                <li className="nav__item">
-                    <button className="nav__button-menu"></button>
-                </li>
             </ul>
-        </nav>
+        </nav >
     )
 }
 
