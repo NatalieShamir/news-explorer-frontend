952dbf906d9afeb54c82ceb29540a0728d9e0218
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2023-07-13 12:30:16 +0300

    change div tag to section

--- src/components/SavedNewsHeader/SavedNewsHeader.js
+++ src/components/SavedNewsHeader/SavedNewsHeader.js
@@ -4,13 +4,13 @@ import "../SavedNewsHeader/SavedNewsHeader.css";
 function SavedNewsHeader() {
     return (
         <>
-            <div className="saved-news-header">
+            <section className="saved-news-header">
                 <h2 className="saved-news-header__heading">Saved articles</h2>
                 <h3 className="saved-news-header__subheading">Elise, you have 5 saved articles</h3>
                 <p className="saved-news-header__keywords">By keywords:
-                    <span className="saved-news-header__keywords-bold" style={{ fontWeight: 'bold' }}>Nature, Yellowstone, and 2 other</span>
+                    <span className="saved-news-header__keywords-bold" >Nature, Yellowstone, and 2 other</span>
                 </p>
-            </div>
+            </section>
         </>
     )
 }
