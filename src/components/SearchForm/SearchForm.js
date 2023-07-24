bdce0823046732eadb241c4633a1ab3311cb13a5
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2023-07-24 11:53:38 +0300

    create and add onChange event handler to input

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -5,13 +5,24 @@ function SearchForm() {
 
     const [searchTopic, setSearchTopic] = React.useState("");
 
+    const handleInput = (e) => {
+        setSearchVal(e.target.value);
+    }
+
     return (
         <div className="search">
             <form className="search__form">
                 <h1 className="search__form-heading">What's going on in the world?</h1>
                 <p className="search__form-text">Find the latest news on any topic and save them in your personal account.</p>
                 <div className="search__form-field-container">
-                    <input type="search" id="search" className="search__form-input" placeholder="Enter topic" value={searchTopic} />
+                    <input
+                        onChange={handleInput}
+                        type="search"
+                        id="search"
+                        className="search__form-input"
+                        placeholder="Enter topic"
+                        value={searchTopic}
+                    />
                     <button className="seacrh__form-button">Search</button>
                 </div>
             </form>
