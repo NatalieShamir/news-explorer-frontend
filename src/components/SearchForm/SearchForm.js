ea7e3ee440752333bac88a32ec84f2da5c9066b0
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2023-07-24 11:41:04 +0300

    refactor: change input to self-closing tag

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -11,7 +11,7 @@ function SearchForm() {
                 <h1 className="search__form-heading">What's going on in the world?</h1>
                 <p className="search__form-text">Find the latest news on any topic and save them in your personal account.</p>
                 <div className="search__form-field-container">
-                    <input type="search" id="search" className="search__form-input" placeholder="Enter topic">{keyword}</input>
+                    <input type="search" id="search" className="search__form-input" placeholder="Enter topic" value={searchTopic} />
                     <button className="seacrh__form-button">Search</button>
                 </div>
             </form>
