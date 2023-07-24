0b63b31c182fd63d3bb33277add3027964d2b6c0
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2023-07-24 22:46:04 +0300

    code functionality for conditionally rendering SearchResults

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,9 +3,11 @@ import Parent from "../Parent/Parent";
 import Header from "../Header/Header";
 import SearchForm from "../SearchForm/SearchForm";
 import About from "../About/About";
-
+import { useLocation } from "react-router-dom";
+import SearchResults from "../SearchResults/SearchResults";
 
 function Main({ onSigninClick, isLoggedIn, username, onLogout }) {
+    const location = useLocation();
     return (
         <>
             <Parent>
@@ -17,6 +19,8 @@ function Main({ onSigninClick, isLoggedIn, username, onLogout }) {
                 />
                 <SearchForm />
             </Parent>
+            {!location.pathname === "/articles" &&
+                <SearchResults />}
             <About />
         </>
     )
