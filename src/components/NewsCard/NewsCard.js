2ea5aaeaa38eff5d9ee15416c8b7e4be8df8c8a7
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2023-07-27 10:34:17 +0300

    add card__tooltip button and change props

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,17 +1,18 @@
 import React from "react";
 import "../NewsCard/NewsCard.css";
 
-function NewsCard(props) {
+function NewsCard({ cardImage, date, title, text, website }) {
 
     return (
         <div className="card">
-            <img src={props.cardImage} className="card__image " alt="news card" />
-            <button className="card__button"></button>
+            <img src={cardImage} className="card__image " alt="news card" />
+            <button type="button" className="card__button"></button>
+            <button type="submit" className="card__tooltip">Sign in to save articles</button>
             <div className="card__container">
-                <p className="card__date">{props.date}</p>
-                <h2 className="card__title">{props.title}</h2>
-                <p className="card__text">{props.text}</p>
-                <p className="card__website">{props.website}</p>
+                <p className="card__date">{date}</p>
+                <h2 className="card__title">{title}</h2>
+                <p className="card__text">{text}</p>
+                <p className="card__website">{website}</p>
             </div>
         </div>
     )
