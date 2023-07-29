ee4ef3bc8882f3eb88d6da0384a922462e5f9741
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2023-07-29 15:50:00 +0300

    refactor card rendering functionality

--- src/components/SavedNews/SavedNews.js
+++ src/components/SavedNews/SavedNews.js
@@ -3,14 +3,26 @@ import SavedNewsCard from "../SavedNewsCard/SavedNewsCard.js";
 import "../SavedNews/SavedNews.css";
 
 function SavedNews({ savedArticles }) {
+    const [showSavedCards, setShowSavedCards] = React.useState(0);
 
-    const cardComponents = savedArticles.map((article) => <SavedNewsCard {...article} key={article._id} />);
+    function handleShowMoreSavedCards() {
+        setShowSavedCards(showSavedCards + 3);
+    }
 
     return (
         <section className="saved-news">
-            <ul className="saved-news__card-list">
-                {React.Children.toArray(cardComponents)}
-            </ul><div>{savedArticles}</div>
+            <ul className="search-results__card-list">
+                {""}
+                {savedArticles.slice(0, 3 + showCards).map((article) => {
+                    return (
+                        <SavedNewsCard {...article}
+                            key={article._id}
+                        />
+                    )
+                })}
+            </ul>
+            {showSavedCards < savedArticles.length && (<button type="button" className="saved-news__button" onClick={handleShowMoreSavedCards}>Show more</button>
+            )}
         </section >
     )
 }
