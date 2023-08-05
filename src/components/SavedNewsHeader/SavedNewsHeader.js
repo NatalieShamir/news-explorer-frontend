5b79f60df46d58fc3cf3c3b928962c3211a934e8
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2023-08-05 16:20:23 +0300

    implement storage length prop to display number of saved articles

--- src/components/SavedNewsHeader/SavedNewsHeader.js
+++ src/components/SavedNewsHeader/SavedNewsHeader.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "../SavedNewsHeader/SavedNewsHeader.css";
 
-function SavedNewsHeader() {
+function SavedNewsHeader({ }) {
+    const savedArticlesNumber = localStorage.length;
     return (
         <>
             <section className="saved-news-header">
                 <h2 className="saved-news-header__heading">Saved articles</h2>
-                <h3 className="saved-news-header__subheading">Elise, you have 5 saved articles</h3>
+                <h3 className="saved-news-header__subheading">Elise, you have {savedArticlesNumber} saved articles</h3>
                 <p className="saved-news-header__keywords">By keywords:
                     <span className="saved-news-header__keywords-bold" >Nature, Yellowstone, and 2 other</span>
                 </p>
