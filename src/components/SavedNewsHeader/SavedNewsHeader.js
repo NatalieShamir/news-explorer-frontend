392d5b8bc4f2c35fa4b14e3772b71e06d7ab37c5
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2023-08-07 12:08:12 +0300

    code displaying current user name implementing useContext hook

--- src/components/SavedNewsHeader/SavedNewsHeader.js
+++ src/components/SavedNewsHeader/SavedNewsHeader.js
@@ -1,13 +1,17 @@
 import React from "react";
+import { useContext } from "react";
 import "../SavedNewsHeader/SavedNewsHeader.css";
+import { UserContext } from "../../contexts/CurrentUserContext";
 
-function SavedNewsHeader({ }) {
+
+function SavedNewsHeader() {
+    const currentUser = useContext(UserContext);
     const savedArticlesNumber = localStorage.length;
     return (
         <>
             <section className="saved-news-header">
                 <h2 className="saved-news-header__heading">Saved articles</h2>
-                <h3 className="saved-news-header__subheading">Elise, you have {savedArticlesNumber} saved articles</h3>
+                <h3 className="saved-news-header__subheading">{currentUser.name}, you have {savedArticlesNumber} saved articles</h3>
                 <p className="saved-news-header__keywords">By keywords:
                     <span className="saved-news-header__keywords-bold" >Nature, Yellowstone, and 2 other</span>
                 </p>
