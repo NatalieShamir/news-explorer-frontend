38bfc16ee591d58f23e47f8bafb2a6cbe54b0a46
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2023-12-24 13:20:27 +0200

    change Navigate to path and remove redundant code

--- src/components/Protected/Protected.js
+++ src/components/Protected/Protected.js
@@ -1,8 +1,7 @@
-import React from 'react'
 import { Navigate } from 'react-router-dom'
 function Protected({ isLoggedIn, children }) {
     if (!isLoggedIn) {
-        return <Navigate to="/" replace />
+        return <Navigate to="/signin" replace />
     }
     return children
 }
