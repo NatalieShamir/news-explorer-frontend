62ea24d433c7bcb13132006ef67652caa001822e
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2024-01-01 14:01:01 +0200

    remove redundant code and correct prop names

--- src/components/SavedNews/SavedNews.js
+++ src/components/SavedNews/SavedNews.js
@@ -1,42 +1,39 @@
 import React from "react";
 import SavedNewsCard from "../SavedNewsCard/SavedNewsCard.js";
 import "../SavedNews/SavedNews.css";
-import Header from "../Header/Header.js";
+import LoggedinHeader from "../LoggedinHeader/LoggedinHeader.js";
 import SavedNewsHeader from "../SavedNewsHeader/SavedNewsHeader.js";
 
-function SavedNews({ savedArticles, onArticleDelete, isLoggedIn, name }) {
-    const [showSavedCards, setShowSavedCards] = React.useState(0);
-
-    function handleShowMoreSavedCards() {
-        setShowSavedCards(showSavedCards + 3);
-    }
-
+function SavedNews({ savedArticles, onArticleDelete, isLoggedIn, name, onLogout }) {
     return (
         <>
-            <Header />
-            <SavedNewsHeader savedArticles={savedArticles}
+            <LoggedinHeader
+                name={name}
+                onLogout={onLogout}
+            />
+            <SavedNewsHeader
+                savedArticles={savedArticles}
                 name={name}
             />
             <section className="saved-news">
                 <ul className="saved-news__card-list">
+                    {" "}
                     {savedArticles.map((savedArticle) => {
                         return (
                             <SavedNewsCard
                                 savedArticle={savedArticle}
-                                key={savedArticle.title + savedArticle.publishedAt}
-                                cardImage={savedArticle.urlToImage}
-                                date={savedArticle.publishedAt}
+                                key={savedArticle.title + savedArticle.date}
+                                cardImage={savedArticle.image}
+                                date={savedArticle.date}
                                 title={savedArticle.title}
-                                text={savedArticle.description}
-                                website={savedArticle.source.name}
+                                text={savedArticle.text}
+                                website={savedArticle.source}
                                 isLoggedIn={isLoggedIn}
                                 onArticleDelete={onArticleDelete}
                             />
                         )
                     })}
                 </ul>
-                {showSavedCards < savedArticles.length && (<button type="button" className="saved-news__button" onClick={handleShowMoreSavedCards}>Show more</button>
-                )}
             </section >
         </>
     )
