45008df5caad96f777de8e5d58054455047e9e8d
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2024-01-02 13:16:35 +0200

    add check to define whether savedArticles is undefined or an empty array

--- src/components/SavedNewsHeader/SavedNewsHeader.js
+++ src/components/SavedNewsHeader/SavedNewsHeader.js
@@ -2,6 +2,14 @@ import React from "react";
 import "../SavedNewsHeader/SavedNewsHeader.css";
 
 function SavedNewsHeader({ savedArticles, name }) {
+    if (!savedArticles || savedArticles.length === 0) {
+        return (
+            <section className="saved-news-header">
+                <h2 className="saved-news-header__heading">Saved articles</h2>
+                <h3 className="saved-news-header__subheading">{name}, you have 0 saved articles</h3>
+            </section>
+        );
+    }
 
     const savedArticlesNumber = savedArticles.length;
     const keywordsArray = savedArticles.map((article) => article.keyword);
@@ -18,37 +26,37 @@ function SavedNewsHeader({ savedArticles, name }) {
 
     let keywordsCountArray = [];
     for (let keyword in keywordsCount) {
-        keywordsCountArray.push({ 'keyword': keyword, 'count': keywordsCount[keyword] })
+        keywordsCountArray.push({ keyword: keyword, count: keywordsCount[keyword] });
     }
 
     const keywordsDescending = keywordsCountArray
         .sort((a, b) => b.count - a.count)
-        .map(element => element.keyword);
+        .map((element) => element.keyword);
 
     let displayKeywords = [];
 
     const keywordsShown = `${keywordsDescending[0]}, ${keywordsDescending[1]}`;
 
-    const diff = keywordsDescending.filter(x => !keywordsShown.includes(x));
+    const diff = keywordsDescending.filter((x) => !keywordsShown.includes(x));
 
-    const remainingKeywordsCount = diff.length
+    const remainingKeywordsCount = diff.length;
 
     if (keywordsDescending.length > 2) {
         displayKeywords = keywordsShown + `\u0020 and ${remainingKeywordsCount} other`;
+    } else {
+        displayKeywords = keywordsShown;
     }
 
     return (
-        <>
-            <section className="saved-news-header">
-                <h2 className="saved-news-header__heading">Saved articles</h2>
-                <h3 className="saved-news-header__subheading">{name}, you have {savedArticlesNumber} saved articles</h3>
-                <p className="saved-news-header__keywords">By keywords:&nbsp;
-                    <span className="saved-news-header__keywords-bold" >{displayKeywords}</span>
-                </p>
-            </section>
-        </>
-    )
-
+        <section className="saved-news-header">
+            <h2 className="saved-news-header__heading">Saved articles</h2>
+            <h3 className="saved-news-header__subheading">{name}, you have {savedArticlesNumber} saved articles</h3>
+            <p className="saved-news-header__keywords">
+                By keywords:&nbsp;
+                <span className="saved-news-header__keywords-bold">{displayKeywords}</span>
+            </p>
+        </section>
+    );
 }
 
 export default SavedNewsHeader;
