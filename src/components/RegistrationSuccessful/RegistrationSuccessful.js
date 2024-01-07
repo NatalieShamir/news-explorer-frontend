a40496fd3b84e4c65b6f49cc97a6bf2ff6bd00b4
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2024-01-07 09:57:14 +0200

    pass onSigninClick prop to onClick handler

--- src/components/RegistrationSuccessful/RegistrationSuccessful.js
+++ src/components/RegistrationSuccessful/RegistrationSuccessful.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../RegistrationSuccessful/RegistrationSuccessful.css"
 
-function RegistrationSuccessful({ isOpen, onClose }) {
+function RegistrationSuccessful({ isOpen, onClose, onSigninClick }) {
     return (
         <div className={`${`registration-success`} ${isOpen ? "registration-success_open" : ""}`}>
             <div className="registration-success__container">
@@ -16,7 +16,7 @@ function RegistrationSuccessful({ isOpen, onClose }) {
                     <div className="registration-success__info">
                         <h3 className="registration-success__status">Registration successfully completed!</h3>
                         <Link style={{ textDecoration: 'none' }} >
-                            <p className="registration-success__link"> Sign in</p>
+                            <p onClick={onSigninClick} className="registration-success__link"> Sign in</p>
                         </Link>
                     </div>
                 </form>
